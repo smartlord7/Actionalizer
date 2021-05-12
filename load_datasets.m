b05bcb5bc4dc7a15790ede08b86078e435ad5581
SamuelCarinhas <samuelsantos.c.2001@gmail.com>
2021-05-12 20:44:30 +0100

    MAIN: Add dataset 1 plot

--- load_datasets.m
+++ load_datasets.m
@@ -1,12 +1,27 @@
-%%file load_datasets.m
+filename = 'datasets/acc_exp01_user01.txt';
+labelname = 'datasets/labels.txt';
+dataset = dlmread(filename, ' ');
+label = dlmread(labelname, ' ');
 
-function datasets = load_datasets(directory)
-    dir_files = files(directory);
-    datasets = zeros(length(dir_files));
-    i = 0;
+colors = ['-r', '-g', '-b', '-c', '-m', '-y', '-r', '-g', 'b', 'c', 'm'];
 
-    for file = dir_files
-        datasets(i) = load(file);    
-        i = i + 1;    
+for k=1:3
+    subplot(3, 1, k);
+    plot(1:length(dataset), dataset(:, k), '-k')
+    hold on
+    for i=1:length(label)
+        exp = label(i, 1);
+        if exp == 2
+            break
+        end
+        user = label(i, 2);
+        activity = label(i, 3);
+        start = label(i, 4);
+        finish = label(i, 5);
+        
+        x = start:finish;
+        plot(x, dataset(x, k), colors(activity))
     end
-end 
+end
+
+hold off
