5a935ea781084ceb5b806f245bd79d74185da4be
João Artur <jfgartur@hotmail.com>
2021-05-21 20:29:20 +0100

    MAIN: Add functions documentation

--- functions/util/get_abs_slope.m
+++ functions/util/get_abs_slope.m
@@ -1,3 +1,16 @@
+%{
+@def get_abs_slope
+@brief Function that gets the slope of the linear regression for a certain
+interval.
+
+@param domain
+Interval of values on which the linear regression will be applied.
+
+@param signal
+Vector with the values to consider.
+
+@return slope - slope of the 1st degree polynomial resultant of the linear regression applied on the signal.
+%}
 function slope = get_abs_slope(domain, signal)
     slope = polyfit(domain, signal, 1);
     slope = abs(slope(1));
